dce3addf0b47de42fa22c869ddad5830077fb2ef
abraham-mendez-code <abraham.me001@gmail.com>
2025-10-14 09:35:11 -0400

    Added LedgerApp class to store UI elements. Added LedgerApp.deposit() to get deposit information and add the transaction to the ledger.

---
+++ src/main/java/com/pluralsight/LedgerApp.java
@@ -0,0 +1,78 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class LedgerApp {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void home() throws InterruptedException {
+
+        String options = """
+                
+                The following options are available
+                    D) Add Deposit
+                    P) Make Payment (Debit)
+                    L) Ledger
+                    X) Exit
+                Enter a command: """;
+
+        System.out.print(options);
+
+        String command = scanner.nextLine().substring(0,1).toLowerCase();
+
+        switch (command) {
+            case "d":
+                deposit();
+                break;
+            case "p":
+                // payment();
+                break;
+            case "l":
+                //
+                break;
+            case "x":
+                System.exit(0);
+            default:
+                System.out.println("Invalid Selection!");
+                Thread.sleep(1000);
+                home();
+        }
+
+    }
+
+    public static void deposit() throws InterruptedException {
+
+        System.out.print("Enter a description(e.g Invoice 66 paid): ");
+        String description = scanner.nextLine().trim();
+
+        System.out.print("Enter a vendor(e.g Sheev): ");
+        String vendor = scanner.nextLine().trim();
+
+        double amount;
+
+        while (true) {
+            System.out.print("Enter an amount: ");
+            try {
+                amount = Double.parseDouble(scanner.nextLine());
+
+                if (amount > 0) {
+                    break;
+                } else {
+                    System.out.printf("$.2f is not a valid deposit", amount);
+                    Thread.sleep(1000);
+                }
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Enter a valid number");
+                Thread.sleep(1000);
+            }
+        }
+
+        Transaction deposit = new Transaction(description, vendor, amount);
+
+        System.out.printf("Deposit for %s made from %s for $%.2f\n", description, vendor, amount);
+
+    }
+
+}
--- src/main/java/com/pluralsight/tester.java
+++ src/main/java/com/pluralsight/tester.java
@@ -4,25 +4,12 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import static com.pluralsight.LedgerApp.*;
+
 public class tester {
-    public static void main(String[] args) throws IOException {
-        Ledger ledger = new Ledger();
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        ArrayList<Transaction> transactions = ledger.getLedger();
-
-        transactions = Reports.getByVendor(transactions, "Starbucks");
-
-        LocalDate startDate = LocalDate.of(2022, 12, 1);
-        LocalDate endDate = LocalDate.of(2025, 10, 12);
-
-        for (Transaction t : transactions ) {
-            System.out.print(t.getDate() + "|");
-            System.out.print(t.getTime() + "|");
-            System.out.print(t.getDescription() + "|");
-            System.out.print(t.getVendor() + "|");
-            System.out.print(t.getAmount() + "|\n");
-
-        }
+            home();
 
     }
 }
